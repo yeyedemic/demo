72565f532550fb362c8141ad4e43618966442c2d
zdx <1913661797@qq.com>
2023-10-08 10:46:37 +0800

    预计会失败

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-console */
 import React from 'react';
 import './App.css';
 import { Button } from 'antd';
 import { getLoginQrCKey } from './api';
 
 console.log(process.env.NODE_ENV);
+
+const a = 59;
 
 function App() {
   const fn = () => {
